cba2f67cc793ff4485b83b8a70979452b8fb605e
jkatungage1 <jacques.katunga-mukendi@universite-paris-saclay.fr>
2025-04-15 22:41:57 +0200

    Python

--- Python/Test/try_block.py
+++ Python/Test/try_block.py
@@ -1,9 +1,9 @@
 import os
 
-try : 
-    if os.path.exists('test.txt') :
+try:
+    if os.path.exists('test.txt'):
         print('File exists')
-    else :
+    else:
         print('File does not exist')
-except :
-    print('An error occurred')  
+except Exception as e:
+    print(f'The error {e} occurred')
